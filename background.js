df9dc297adfba107775c940ac1ab151be3a22630
kgsensei <jt@kellem.com>
2023-08-29 14:00:54 -0400

    Updated Everything

--- background.js
+++ background.js
@@ -1,59 +1,165 @@
+const storage = {
+    getItem: async (key) => {
+        return (await chrome.storage.local.get(key))[key]
+    },
+    setItem: (key, val) => {
+        chrome.storage.local.set({[key]:val})
+    },
+    removeItem: (keys) => {
+        chrome.storage.local.remove(keys)
+    }
+}
+
 function update(type, data) {
-    console.log("update")
-	// Background size
+	// Background color
     if(type == 'bg') {
-		document.body.style.backgroundColor = `rgb(${data}) !important`
+		document.body.style.backgroundColor = `rgb(${data})`
+        // Update divs
 		let e = document.getElementsByTagName('div')
 		for(let i = 0; i < e.length; i++) {
-			e[i].style.backgroundColor = `rgb(${data}) !important`
+			e[i].style.backgroundColor = `rgb(${data})`
+		}
+        // Update h1 tags
+        let f = document.getElementsByTagName('h1')
+		for(let i = 0; i < f.length; i++) {
+			f[i].style.backgroundColor = `rgb(${data})`
+		}
+        // Update h2 tags
+        let g = document.getElementsByTagName('h2')
+		for(let i = 0; i < g.length; i++) {
+			g[i].style.backgroundColor = `rgb(${data})`
+		}
+        // Update h3 tags
+        let h = document.getElementsByTagName('h3')
+		for(let i = 0; i < h.length; i++) {
+			h[i].style.backgroundColor = `rgb(${data})`
+		}
+        // Update h4 tags
+        let j = document.getElementsByTagName('h4')
+		for(let i = 0; i < j.length; i++) {
+			j[i].style.backgroundColor = `rgb(${data})`
 		}
 	}
+
     // Font color
 	if(type == 'fg') {
-		document.body.style.color = `rgb(${data}) !important`
+		document.body.style.color = `rgb(${data})`
+        // Update divs
 		let e = document.getElementsByTagName('div')
 		for(let i = 0; i < e.length; i++) {
-			e[i].style.color = `rgb(${data}) !important`
+			e[i].style.color = `rgb(${data})`
+		}
+        // Update spans
+        let f = document.getElementsByTagName('span')
+		for(let i = 0; i < f.length; i++) {
+			f[i].style.color = `rgb(${data})`
 		}
 	}
+
     // Font size
 	if(type == 'fs') {
-		document.body.style.fontSize = `${data}px !important`
+		document.body.style.fontSize = `${data}px`
 		let e = document.getElementsByTagName('p')
 		for(let i = 0; i < e.length; i++) {
-			e[i].style.fontSize = `${data}px !important`
+			e[i].style.fontSize = `${data}px`
 		}
 	}
+
     // Link color
     if(type == 'ln') {
 		let e = document.getElementsByTagName('a')
 		for(let i = 0; i < e.length; i++) {
-			e[i].style.color = `rgb(${data}) !important`
-			e[i].style.textDecorationLine = 'underline !important'
+			e[i].style.color = `rgb(${data})`
+			e[i].style.textDecorationLine = 'underline'
 		}
 	}
+
     // Font family
     if(type == 'ff') {
-		document.body.style.fontSize = `${data}px !important`
+		document.body.style.fontFamily = `${data}`
 		let e = document.getElementsByTagName('p')
 		for(let i = 0; i < e.length; i++) {
-			e[i].style.fontSize = `${data}px !important`
+			e[i].style.fontFamily = `${data}`
 		}
 	}
+
     return true
+}
+
+async function getDomain() {
+    let x = await chrome.tabs.query({active: true})
+    if(x[0].url == undefined) {
+        return false
+    }
+    let u = new URL(x[0].url)
+    return u.hostname.replaceAll(".", "-")
+}
+
+async function removeTheme() {
+    let x = await getDomain()
+    if(x == false) {
+        return
+    }
+    storage.removeItem(x)
+}
+
+async function saveTheme(data) {
+    let x = await getDomain()
+    if(x == false) {
+        return
+    }
+    let d = JSON.stringify(data)
+    storage.setItem(x, d)
+}
+
+async function loadSaved(i) {
+    let u = await getDomain()
+    storage.setItem("currentSite", u || false)
+    if(u == false) {
+        return
+    }
+    let d = await storage.getItem(u)
+    if(d == undefined) {
+        return
+    }
+    d = JSON.parse(d)
+    for (const t in d) {
+        if(t != null) {
+            chrome.scripting.executeScript({
+                target: {
+                    tabId: i,
+                    allFrames: true
+                },
+                func: update,
+                args: [t, d[t]]
+            })
+        }
+    }
 }
 
 chrome.runtime.onMessage.addListener(async (data) => {
 	let x = await chrome.tabs.query({active: true})
 	let ap = JSON.parse(data)
-    console.log(ap)
-    console.log(x[0].id)
-	chrome.scripting.executeScript({
-		target: {
-            tabId: x[0].id,
-            allFrames: true
-        },
-		func: update,
-		args: [ap.type, ap.data]
-	})
+    if(ap.type == "remember") {
+        if(ap.data == true) {
+            saveTheme(ap.settings)
+        } else {
+            removeTheme()
+        }
+    } else {
+        chrome.scripting.executeScript({
+            target: {
+                tabId: x[0].id,
+                allFrames: true
+            },
+            func: update,
+            args: [ap.type, ap.data]
+        })
+    }
+})
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if(changeInfo.status == 'complete') {
+        loadSaved(tabId)
+    }
 })
--- dash/apPlugin.js
+++ dash/apPlugin.js
@@ -1,4 +1,29 @@
+const storage = {
+    getItem: async (key) => {
+        return (await chrome.storage.local.get(key))[key]
+    },
+    setItem: (key, val) => {
+        chrome.storage.local.set({[key]:val})
+    },
+    removeItem: (keys) => {
+        chrome.storage.local.remove(keys)
+    }
+}
+
+const settings = {
+    'bg': null,
+    'fg': null,
+    'ln': null,
+    'fs': null,
+    'ff': null,
+    'ps': null
+}
+
+const toastId = document.getElementById("toast")
+const toast = new bootstrap.Toast(toastId, {'autohide': true, 'delay': 2000})
+
 function send(type, data) {
+    settings[type] = data
 	chrome.runtime.sendMessage(
 		JSON.stringify(
 			{
@@ -17,24 +42,67 @@ function apUpdate() {
 			send('bg', '133,234,113')
 			send('fg', '0,0,255')
 			send('ln', '205,50,50')
-		}
-		if(data == 'B') {
+            send('ff', 'Arial')
+            send('fs', '20')
+		} else if(data == 'B') {
 			send('bg', '54,54,54')
 			send('fg', '0,255,0')
 			send('ln', '205,50,50')
-		}
-		if(data == 'C') {
+            send('ff', 'Arial')
+            send('fs', '20')
+		} else if(data == 'C') {
 			send('bg', '189,181,165')
 			send('fg', '0,0,0')
 			send('ln', '50,50,205')
+            send('ff', '"Comic Sans MS", "Comic Sans", cursive')
+            send('fs', '20')
+		} else if(data == 'D') {
+			send('bg', '20,20,25')
+			send('fg', '255,255,255')
+			send('ln', '0,180,255')
+            send('ff', 'ap_custom_dyslexiaFont')
+            send('fs', '18')
+		} else if(data == 'E') {
+			send('bg', '25,20,20')
+			send('fg', '255,255,255')
+			send('ln', '255,180,0')
+            send('ff', 'ap_custom_dyslexiaFont')
+            send('fs', '18')
 		}
-		send('fs', '22')
 	} else {
 		send(type, data)
 	}
 }
 
-var e = document.getElementsByClassName('option')
-for (var i = e.length; i--; null) {
-    e[i].addEventListener('click', apUpdate, false);
+let e = document.getElementsByClassName('option')
+for (let i = 0; i < e.length; i++) {
+    e[i].addEventListener('click', apUpdate, false)
 }
+
+document.getElementById("remember").addEventListener("click", () => {
+    toastId.innerHTML = "Settings Remembered"
+    toast.show()
+    chrome.runtime.sendMessage(
+        JSON.stringify(
+            {
+                type: 'remember',
+                data: true,
+                settings: settings
+            }
+        )
+    )
+})
+
+document.getElementById("forget").addEventListener("click", () => {
+    toastId.innerHTML = "Settings Forgotten"
+    toast.show()
+    chrome.runtime.sendMessage(
+        JSON.stringify(
+            {
+                type: 'remember',
+                data: false,
+                settings: null
+            }
+        )
+    )
+})
