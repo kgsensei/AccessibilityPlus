a129d6562825766e9212674cc8d2154da746631e
kgsensei <jt@kellem.com>
2024-03-09 14:05:53 -0500

    Add ka support

--- dash/apPlugin.js
+++ dash/apPlugin.js
@@ -106,3 +106,9 @@ document.getElementById("forget").addEventListener("click", () => {
         )
     )
 })
+
+fetch("http://localhost/api/software", {
+    "method": "POST",
+    "headers": { "Content-Type": "application/json" },
+    "body": JSON.stringify({ project: "AccessibilityPlus" })
+})
--- firefox_only/dash/apPlugin.js
+++ firefox_only/dash/apPlugin.js
@@ -100,3 +100,9 @@ document.getElementById("forget").addEventListener("click", () => {
         )
     )
 })
+
+fetch("http://localhost/api/software", {
+    "method": "POST",
+    "headers": { "Content-Type": "application/json" },
+    "body": JSON.stringify({ project: "AccessibilityPlus" })
+})
